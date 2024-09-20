7e02966cd81e58a7eb494c83ebd55cb375812241
deus211 <2023abit1858f@kab.ac.ug>
2024-09-20 04:42:13 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$name="MIREMBE DEUS";
+$DOB="22/09/2001";
+$Address="KISORO";
+$CYear=2024;
+$BYear=2001;
+$Age=$CYear-$BYear;
+echo"Name: ".$name. "<br>";
+echo"Date Of Birth : ".$DOB."<br>";
+echo"Home Address: ".$Address."<br>";
+echo"Welcome home, ".$name."<br>";
+echo"You are now , ".$Age."Years old";
+/* DOB stands for date of birth,
+CYEAR stands for current year
+BYEAR stands for year of birth*/
+?> 
+
